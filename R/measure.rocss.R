733ef2bd292479dc601dfd36009a8f849eff89fb
jbedia <joaquin.bedia@gmail.com>
2019-07-12 13:11:35 +0200

    Document function

--- R/measure.rocss.R
+++ R/measure.rocss.R
@@ -1,19 +1,19 @@
 #' @title Roc Skill Score (ROCSS)
 #' @description Calculate Roc Skill Score (ROCSS)
 #' @author J.Baño-Medina
-#' @param obs A vector containing the observed time series
-#' @param prd A vector containing the predicted time series
-#' @return A float number corresponding to the amount fallen the days above the input threshold.
+#' @template templateMeasureParams
+#' @param dates dates
+#' @return A float number corresponding to the ROCSS
 #' @importFrom verification roc.area
 #' @export
 
-measure.rocss <- function(indexObs = NULL, indexPrd = NULL,obs = NULL, prd = NULL, dates) {
+measure.rocss <- function(indexObs = NULL, indexPrd = NULL, obs = NULL, prd = NULL, dates) {
   if (length(obs) <= 1) stop("Observed time series is needed")
   if (length(prd) <= 1) stop("Predicted time series is needed")
   if (!all(is.na(obs))) {
     tryCatch({
-      roc.area(obs=obs,pred=prd)$A*2 - 1
-    }, error=function(x){
+      roc.area(obs = obs, pred = prd)$A*2 - 1
+    }, error = function(x) {
       NA
     })
   } else {
