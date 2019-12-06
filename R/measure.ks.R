d1c11e7f8b541115b68562bb73bef4d1b36b3d08
jbedia <joaquin.bedia@gmail.com>
2019-12-06 19:07:19 +0100

    Add dates argument for compatibility with climate4R wrapper

--- R/measure.ks.R
+++ R/measure.ks.R
@@ -1,12 +1,13 @@
 #' @title Two-sided KS score
 #' @description Function to compute the (two-sided) Kolmogorov-Smirnov (KS) score between the observed and predicted time series. 
 #' @template templateMeasureParams
+#' @param dates Ignored. Introduced for compatibility with the rest of measures
 #' @return A float number corresponding to the KS score.
 #' @author J. Bedia
 #' @importFrom stats ks.test
 #' @export
 
-measure.ks <- function(indexObs = NULL, indexPrd = NULL, obs, prd) {
+measure.ks <- function(indexObs = NULL, indexPrd = NULL, obs, prd, dates = NULL) {
     if (length(obs) <= 1) stop("Observed time series is required")
     if (length(prd) <= 1) stop("Predicted time series is required")
     unname(ks.test(obs, prd, alternative = "two.sided")$statistic)
